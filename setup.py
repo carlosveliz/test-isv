e6a58947c0faa29dd47dd2564f12140c4bc65939
Carlos Véliz <cxrlos@MacBook-Pro-de-Carlos.local>
2016-09-21 17:16:31 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='consolidador',
+      version='0.1.0',
+      description='Consolidador de descargas',
+      url='https://github.com/carlosveliz/test-isv.git',
+      author='Carlos Veliz',
+      author_email='carlos.veliz@me.com',
+      license='MIT',
+      packages=['consolidador'],
+      zip_safe=False)
